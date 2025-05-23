bcd5d246692e8a96639fc40eb18d2d8b6f94f83b
bahadorimat <bahadorimat@gmail.com>
2025-05-23 09:49:52 +0200

    Update matin bahadori.R

--- matin bahadori.R
+++ matin bahadori.R
@@ -1,5 +1,3 @@
-#Exercise1-----
-
 x<- matrix(runif(100, min=0, max=1), nrow=10,ncol=10)
 x
 df1 <- data.frame(x)
@@ -16,17 +14,3 @@ func <- function(x) {(((x * 3) + 6) / 3) - x}
 func(x)
 
 
-#Exercise2-----
-
-setwd("/Users/matinbahadori/Downloads")
-defo <- read.table("defoliation.txt", header = TRUE)
-sylv<- defo[defo$Specie=="Pinus sylvestris",]
-sylv
-Abundance_cover <- matrix(runif(76, min=0, max=1))
-Abundance_cover
-water <- defo[defo$Water_potential]
-water
-DF1 <- data.frame(Abundance_cover,water)
-DF1
-sylv_DF1 <- merge(sylv, DF1, by = "Water_potential")
-sylv_DF1
